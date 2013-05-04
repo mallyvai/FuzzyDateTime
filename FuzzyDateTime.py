da2a947002acea772711c735bb2bcb48df9ab998
Vaibhav Mallya <vmallya@twitter.com>
2013-05-04 01:50:41 -0700

    Updating some docs

--- FuzzyDateTime.py
+++ FuzzyDateTime.py
@@ -1,5 +1,23 @@
 import datetime
 
+"""
+Implements fuzzy comparison for the DateTime object.
+If two datetime objects are within a particular threshold
+(specified as part of the constructror) they will be
+treated as being equal. If two FuzzyDatetime objects
+have different thresholds, the first one in the comparison
+will have its threshold used.
+
+Identical to standard datetime in all other ways.
+
+Usage Example:
+
+  seconds_threshold = 120
+  first = FuzzyDateTime(seconds_threshold, 2013, 8, 12, 4, 1)
+  second = FuzzyDateTime(seconds_threshold, 2013, 8, 12, 5, 10)
+  first < second # This will evaluate to false since they're within
+  two minutes (120 seconds) of each other
+"""
 class FuzzyDateTime(datetime.datetime):
   def __new__(cls, fuzzy_threshold=0, *args, **kwargs):
     val = super(FuzzyDateTime, cls).__new__(cls, *args, **kwargs)
--- TestFuzzyDateTime.py
+++ TestFuzzyDateTime.py
@@ -3,14 +3,14 @@ import unittest
 
 '''
 Trying something new here- 
-Naming date and time variables is really obnoxious and 
+Naming date and time variables in tests is really obnoxious and 
 weirdly abstract if you're trying to be descriptive. To make it easier to
 reason about, I'm going to name this after presidents/historical figures 
 who we "know", intuitively, are near and far from each other in history.
 '''
 class FuzzyDateTimeTestCase(unittest.TestCase):
   def setUp(self):
-    t = 60*60*24*265*10
+    t = 60*60*24*265*10 # 10 years
     self.lincoln = FuzzyDateTime(t, 1886, 1, 1, 0)
     self.grant = FuzzyDateTime(t, 1892, 1, 1, 0)
     self.clinton = FuzzyDateTime(t, 2000, 1,1,0)
